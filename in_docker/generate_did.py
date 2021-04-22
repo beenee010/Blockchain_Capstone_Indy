d3e5c6ce5204af4310e21d496f558b2e1b2e7ab7
beenee010 <beenee010@naver.com>
2021-04-22 13:02:28 +0900

    First

---
+++ in_docker/generate_did.py
@@ -0,0 +1,85 @@
+"""
+Example demonstrating how to add DID with the role of Trust Anchor to ledger.
+Uses seed to obtain Steward's DID which already exists on the ledger.
+Then it generates new DID/Verkey pair for Trust Anchor.
+Using Steward's DID, NYM transaction request is built to add Trust Anchor's DID and Verkey
+on the ledger with the role of Trust Anchor.
+Once the NYM is successfully written on the ledger, it generates new DID/Verkey pair that represents
+a client, which are used to create GET_NYM request to query the ledger and confirm Trust Anchor's Verkey.
+For the sake of simplicity, a single wallet is used. In the real world scenario, three different wallets
+would be used and DIDs would be exchanged using some channel of communication
+"""
+
+import asyncio
+import json
+import pprint
+
+from indy import pool, ledger, wallet, did
+from indy.error import IndyError, ErrorCode
+
+from utils import get_pool_genesis_txn_path, PROTOCOL_VERSION
+
+pool_name = 'hello'
+genesis_file_path = get_pool_genesis_txn_path(pool_name)
+
+wallet_config = json.dumps({"id": "wallet1"})
+wallet_credentials = json.dumps({"key": "wallet_key"})
+
+def print_log(value_color="", value_noncolor=""):
+    """set the colors for text."""
+    HEADER = '\033[92m'
+    ENDC = '\033[0m'
+    print(HEADER + value_color + ENDC + str(value_noncolor))
+
+
+async def write_nym_and_query_verkey():
+    try:
+        await pool.set_protocol_version(PROTOCOL_VERSION)
+        # 1.
+        print_log('\n1. Creates a new local pool ledger configuration that is used '
+                  'later when connecting to ledger.\n')
+        pool_config = json.dumps({'genesis_txn': str(genesis_file_path)})
+        try:
+            await pool.create_pool_ledger_config(config_name=pool_name, config=pool_config)
+        except IndyError as ex:
+            if ex.error_code == ErrorCode.PoolLedgerConfigAlreadyExistsError:
+                pass
+
+        print_log('genesis_txn: ', genesis_file_path)
+
+        # 2.
+        print_log('\n2. Open pool ledger and get handle from libindy\n')
+        pool_handle = await pool.open_pool_ledger(config_name=pool_name, config=None)
+       
+        # 3.
+        print_log('\n3. Creating new secure wallet\n')
+        try:
+            await wallet.create_wallet(wallet_config, wallet_credentials)
+        except IndyError as ex:
+            if ex.error_code == ErrorCode.WalletAlreadyExistsError:
+                pass
+
+        # 4.
+        print_log('\n4. Open wallet and get handle from libindy\n')
+        wallet_handle = await wallet.open_wallet(wallet_config, wallet_credentials)
+
+        #5.
+        print_log('\n9. Generating and storing DID and verkey representing a Client\n')
+        client_did, client_verkey = await did.create_and_store_my_did(wallet_handle, "{}")
+        print_log('Client DID: ', client_did)
+        print_log('Client Verkey: ', client_verkey)
+
+        # Step 5 code goes here.
+
+    except IndyError as e:
+        print('Error occurred: %s' % e)
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(write_nym_and_query_verkey())
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
