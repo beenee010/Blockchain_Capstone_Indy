65fab99dac217e9a7b76209e38404279f304ce55
beenee010 <beenee010@naver.com>
2021-04-26 16:12:41 +0900

    data.json 경로 변경

--- start_docker/api.sh
+++ start_docker/api.sh
@@ -10,6 +10,6 @@ echo create wallet and did !!
 docker exec -itu 0 "$name" python3 /home/indy/generate_did.py "${email}"
 
 echo file copy !!
-docker cp "$name":/home/indy/data.json /Users/beenie/
+docker cp "$name":/home/indy/data.json ./
 
 echo "<process end>"
