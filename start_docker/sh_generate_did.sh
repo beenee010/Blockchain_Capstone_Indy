cde4842c4259205535e65f1db3aac5302353b281
GyuSeon-Jo <plame114@gmail.com>
2021-05-20 17:36:37 +0900

    학번 파라미터 추가

--- start_docker/sh_generate_did.sh
+++ start_docker/sh_generate_did.sh
@@ -5,13 +5,14 @@
 name="$1"
 wallet_name="$2"
 wallet_key="$3"
+seed="$4"
 
 echo create wallet and did !!
 
 echo "${wallet_name}"
 echo "${wallet_key}"
 
-docker exec -iu 0 "$name" python3 /home/indy/generate_did.py "${wallet_name}" "${wallet_key}"
+docker exec -iu 0 "$name" python3 /home/indy/generate_did.py "${wallet_name}" "${wallet_key}" "${seed}"
 
 echo file copy !!
 docker cp "$name":/home/indy/data.json /home/deploy
