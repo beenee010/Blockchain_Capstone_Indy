82ea363df37c01062e23131b9ad3c2d38efcef5a
GyuSeon-Jo <plame114@gmail.com>
2021-05-20 16:51:58 +0900

    New Wallet ID 출력

---
+++ start_docker/export_wallet_id.py
@@ -0,0 +1,27 @@
+import asyncio
+from collections import OrderedDict
+import json
+import pprint
+import sys
+import re
+
+# Exported DID
+email = sys.argv[1]
+
+# User DID's Seed
+wallet_name = sys.argv[2]
+
+# User Student ID
+student_id = sys.argv[3]
+
+
+# Make Json File to docker
+user_data = OrderedDict()
+user_data["email"] = email
+user_data["new_wallet"] = wallet_name
+        
+print('\nMake User EMail, New Wallet ID Json File\n')
+print(json.dumps(user_data, ensure_ascii=False, indent="\t"))
+
+with open(student_id+'_new_wallet_id.json','w',encoding="utf-8") as make_file:
+    json.dump(user_data, make_file, ensure_ascii=False, indent="\t")
