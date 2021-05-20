e945542ad94f18b4b416471ba2c7bb2f08fe2b83
GyuSeon-Jo <plame114@gmail.com>
2021-05-20 16:52:32 +0900

    Wallet 재발급

---
+++ start_docker/sh_regenerate_did.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# set -e
+
+name="$1"
+did="$2"
+seed="$3"
+email="$4"
+wallet_name="$5"
+wallet_key="$6"
+
+# export did
+docker exec -iu 0 "${name}" python3 /home/indy/export_did.py "${did}" "${seed}"
+
+# did import
+echo "pool connect testpool" >> new_wallet_command.txt
+echo "wallet create ""${wallet_name}" "key=""${wallet_key}" >> new_wallet_command.txt
+echo "wallet open ""${wallet_name}" "key=""${wallet_key}" >> new_wallet_command.txt
+echo "did import /home/indy/""${seed}""did.json" >> new_wallet_command.txt
+echo "wallet close " >> new_wallet_command.txt
+echo "wallet detach" "${wallet_name}" >> new_wallet_command.txt
+echo "exit" >> new_wallet_command.txt
+
+docker cp ./new_wallet_command.txt "${name}":/home/indy
+docker exec -iu 0 "${name}" indy-cli /home/indy/new_wallet_command.txt
+
+# set metadata
+docker exec -iu 0 "${name}" python3 /home/indy/set_metadata.py "${wallet_name}" "${wallet_key}"
+
+# export new wallet id
+docker exec -iu 0 "${name}" python3 /home/indy/export_wallet_id.py "${email}" "${wallet_name}" "${seed}"
+
+rm new_wallet_command.txt
