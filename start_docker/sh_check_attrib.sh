942767dd2c1ee3c21261c8fc0f428b62671bc733
GyuSeon-Jo <plame114@gmail.com>
2021-05-26 21:21:05 +0900

    tx에서 사용자 조회

--- start_docker/sh_check_attrib.sh
+++ start_docker/sh_check_attrib.sh
@@ -14,8 +14,8 @@ echo "<< Process Start >>\n"
 docker exec -iu 0 "$name" python3 /home/indy/check_attrib.py "${admin_did}" "${info_hash}" "${student_id}"
 
 # Copy output file to docker container
-# docker cp "$name":/home/indy/"${student_id}"_check_attrib.json /home/deploy
-docker cp "$name":/home/indy/"${student_id}"_check_attrib.json ./
+docker cp "$name":/home/indy/"${student_id}"_check_attrib.json /home/deploy
+#docker cp "$name":/home/indy/"${student_id}"_check_attrib.json ./
 echo "[Log File Copy Docker to Server]"
 
 # docker exec -iu 0 "$name" rm /home/indy/"${student_id}"_check_attrib.json
